bdb428078c47ac81088bfa9d17cf059f5060f56a
Teme54 <t5lute00@students.oamk.fi>
2018-02-12 11:50:54 +0200

    te

---
+++ reindeerTrackerHardware/reindeerlog/source/adc_func.h
@@ -0,0 +1,16 @@
+/*
+ * adc_func.h
+ *
+ *  Created on: Feb 9, 2018
+ *      Author: teemu
+ */
+
+#ifndef SOURCE_ADC_FUNC_H_
+#define SOURCE_ADC_FUNC_H_
+
+void initAdc();
+unsigned short ADC_read16b(void);
+
+
+
+#endif /* SOURCE_ADC_FUNC_H_ */
