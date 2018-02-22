14c866d1929ab96337c987ca636a6b1d0a92ce62
Teun Verhaert <teun.verhaert1@gmail.com>
2018-02-22 14:00:10 +0100

    Routing module added

---
+++ ReindeerTrackerWeb/src/app/manifacturer/manifacturer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manifacturer',
+  templateUrl: './manifacturer.component.html',
+  styleUrls: ['./manifacturer.component.css']
+})
+export class ManifacturerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
