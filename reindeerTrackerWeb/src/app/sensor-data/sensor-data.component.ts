18937954fd1190d6ff6f0758c18d524937efb73b
Unknown <wauters.maxim@hotmail.com>
2018-03-15 14:01:35 +0100

    Added login layout

--- reindeerTrackerWeb/src/app/sensor-data/sensor-data.component.ts
+++ reindeerTrackerWeb/src/app/sensor-data/sensor-data.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SensorDataService } from './sensor-data.service';
+import { HttpClient } from 'selenium-webdriver/http';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-sensor-data',
@@ -8,17 +10,10 @@ import { SensorDataService } from './sensor-data.service';
   providers: [SensorDataService]
 })
 export class SensorDataComponent implements OnInit {
-
-  lat: number;
-  long: number;
-
+naam : string;
   constructor(private server: SensorDataService) { }
 
   ngOnInit() {
-/*
-    this.server.getSensorData().subscribe(data => this.lat = data);
-    this.server.getSensorData().subscribe(data => this.long = data);
-*/
+    this.server.getSensorData().subscribe(response => this.naam = response.firstName);
   }
-
 }
--- reindeerTrackerWeb/src/app/sensor-data/sensor-data.service.ts
+++ reindeerTrackerWeb/src/app/sensor-data/sensor-data.service.ts
@@ -1,20 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from "@angular/http";
-import {HttpClientModule, HttpClient} from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
 
 @Injectable()
 export class SensorDataService {
 
-  private _ip: string = "ip adress server";
+  private _ip: string = "http://localhost:3000/api/users/fff/devices";
 
-
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: Http) { }
 
   getSensorData() {
-    return this._http.get(this._ip).map((response => response));
+    return this._http.get(this._ip).map((response => response.json()));
 }
-
 
 }
