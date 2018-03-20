e24e516699a6aadb05693bd227585ea1c9f5f23d
Teun Verhaert <teun.verhaert1@gmail.com>
2018-03-20 18:43:59 +0100

    device add and qr scan completed

--- reindeerTrackerApp/src/app/app.component.ts
+++ reindeerTrackerApp/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LogInPage } from '../pages/log-in/log-in';
+
+import * as firebase from 'firebase';
+import {firebaseConfig} from "./app.module";
 @Component({
   templateUrl: 'app.html'
 })
@@ -14,8 +17,10 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      firebase.initializeApp(firebaseConfig);
       statusBar.styleDefault();
       splashScreen.hide();
+
     });
   }
 }
